0a5f1cc0cce79499af6eb7144644aead30f5120b
lifangzheng <1397118980@qq.com>
2025-04-08 11:34:33 +0800

    feat: 集成less 和tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
+  css: {
+    modules: {
+      // 配置 CSS Modules 的类名生成规则
+      generateScopedName: '[name]__[local]___[hash:base64:5]',
+    },
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true
+      }
+    }
+  },
 })
