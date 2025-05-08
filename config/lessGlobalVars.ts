f8830dcb6d92ef2ff162e0963affa8bf4f4ec6c8
lifangzheng <1397118980@qq.com>
2025-05-08 18:27:21 +0800

    feat: 重置

--- config/lessGlobalVars.ts
+++
@@ -1,9 +0,0 @@
-// less全局变量
-const lessVars: Record<string, string> = {
-  'primary-color': '#1890ff',
-}
-
-export { lessVars }
-
-
-
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { lessVars } from './config/lessGlobalVars.ts'
 import path from 'path'
 
 const resolve = (paths: string) => {
@@ -10,7 +8,7 @@ const resolve = (paths: string) => {
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [react()],
   // 配置路径别名
   resolve: {
     alias: {
@@ -19,21 +17,6 @@ export default defineConfig({
       '@/components': resolve('src/components'),
       '@/styles': resolve('src/styles'),
       '@/utils': resolve('src/utils')
-    }
-  },
-  css: {
-    modules: {
-      // 配置 CSS Modules 的类名生成规则
-      generateScopedName: '_[local]_[hash:base64:5]',
-      // 配置不需要进行 CSS Modules 的文件
-      globalModulePaths: []
-    },
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
-        // less配置全局变量
-        globalVars: lessVars
-      }
     }
   },
   build: {
