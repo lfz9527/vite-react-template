ce55af511a945097d97725f3dc95e2ad655897bb
lifangzhegn <lifangzheng@jy.com>
2026-02-05 10:19:28 +0800

    feat: 添加新复制函数

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -2,3 +2,24 @@ import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 // 类名合并
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
+
+/**
+ * 复制文本到剪贴板
+ * @param text
+ * @returns
+ */
+export const clipboard = (text: string | number) => {
+  return new Promise((resolve, reject) => {
+    if (!navigator?.clipboard?.writeText) {
+      reject()
+    }
+    navigator.clipboard
+      .writeText(text.toString())
+      .then(() => {
+        resolve(true)
+      })
+      .catch((err) => {
+        reject(err)
+      })
+  })
+}
